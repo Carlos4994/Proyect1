7bb58add5c55406aab2623f61a7d6f70e44e22f9
Carlos4994 <50203745+Carlos4994@users.noreply.github.com>
2023-08-14 19:19:20 -0500

    Update universe.sql

--- universe.sql
+++ universe.sql
@@ -1,9 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
--- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
